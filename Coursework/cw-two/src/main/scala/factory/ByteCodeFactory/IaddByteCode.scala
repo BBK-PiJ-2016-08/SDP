3547cf463f9d18b9873c2e4ef427e36cfbd255a6
Jake Holdom <jakeholdombbk@gmail.com>
2017-04-17 15:52:22 +0100

    Reimplemented the factory classes. Started on part 2.

---
+++ Coursework/cw-two/src/main/scala/factory/ByteCodeFactory/IaddByteCode.scala
@@ -0,0 +1,32 @@
+package factory.ByteCodeFactory
+
+import bc.{ByteCode, ByteCodeFactory, ByteCodeValues, InvalidBytecodeException}
+import vm.VirtualMachine
+
+/**
+  * Created by jakeholdom on 17/03/2017.
+  */
+class IaddByteCode extends ByteCode{
+  /**
+    * A unique byte value representing the bytecode. An implementation
+    * will set this to the bytecode corresponding to the name of the
+    * bytecode in [[ByteCodeValues]]
+    */
+  override val code: Byte = bytecode("iadd")
+
+  /**
+    * Returns a new [[VirtualMachine]] after executing this bytecode operation.
+    *
+    * @param vm the initial virtual machine
+    * @return a new virtual machine
+    */
+  override def execute(vm: VirtualMachine): VirtualMachine = {
+
+    val x = vm.pop()._1
+    val y = vm.pop()._1
+
+    vm.push(x + y)
+
+  }
+}
+
---
+++ Coursework/cw-two/src/test/scala/ownTests/PublicOwnByteCodeFactorySuite.scala
@@ -0,0 +1,8 @@
+package ownTests
+
+/**
+  * Created by jakeholdom on 17/04/2017.
+  */
+class PublicOwnByteCodeFactorySuite {
+
+}
