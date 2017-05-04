f1f0c9a889b424d827c885fc7bb7e7d3ab97431a
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-05-04 08:05:41 +0100

    Added description comments.

--- coursework/cw-two/src/main/scala/vm/VirtualMachineParserImpl.scala
+++ coursework/cw-two/src/main/scala/vm/VirtualMachineParserImpl.scala
@@ -19,9 +19,6 @@ class VirtualMachineParserImpl extends VirtualMachineParser with ByteCodeValues{
     * @param file the file containing a program
     * @return a vector of bytecodes
     */
-  @throws(classOf[NoSuchElementException])
-  @throws(classOf[NumberFormatException])
-  @throws(classOf[ArrayIndexOutOfBoundsException])
   @throws(classOf[bc.InvalidBytecodeException])
   override def parse(file: String): Vector[ByteCode] = {
     val programParser = new ProgramParserImpl()
@@ -38,8 +35,6 @@ class VirtualMachineParserImpl extends VirtualMachineParser with ByteCodeValues{
     ByteCodeVector
   }
 
-
-
   /**
     * Returns a vector of [[bc.ByteCode]].
     *
@@ -55,33 +50,37 @@ class VirtualMachineParserImpl extends VirtualMachineParser with ByteCodeValues{
     var byteCodes = Vector[ByteCode]()
     val fields = str.split("\n")
 
-      if (fields.nonEmpty) {
-        for (field <- fields) {
-          val fieldOne = field
-          if (field.contains(" ")) {
+    //Check whether the file to parse is empty
+    if (fields.nonEmpty) {
+      for (field <- fields) { // Loop through all the fields of the file
+        val fieldOne = field
+        if (field.contains(" ")) {//
 
-            val multipleString = field.split(" ")
-            val fieldOne = bytecode(multipleString(0))
-            val fieldTwo = multipleString(1).toByte
-            if(validateByteCode(multipleString(0)) && multipleString(1).toInt > 0){
+          val multipleString = field.split(" ")
+          val fieldOne = bytecode(multipleString(0))
+          val fieldTwo = multipleString(1).toByte
+          if(validateByteCode(multipleString(0)) && multipleString(1).toInt > 0){
 
-              byteCodes ++= byteCodeParser.parse(Vector(fieldOne, fieldTwo))
+            //appends the fields onto the vector of byte codes
+            byteCodes ++= byteCodeParser.parse(Vector(fieldOne, fieldTwo))
 
-            }else{
-              throw new bc.InvalidBytecodeException("Error: Invalid byte code")
-            }
-          } else {
-            if (validateByteCode(field)) {
-              byteCodes ++= byteCodeParser.parse(Vector(bytecode(fieldOne)))
-            }else{
-              throw new bc.InvalidBytecodeException("Error: Invalid byte code")
-            }
-
+          }else{
+            //An exception representing an invalid bytecode is thrown
+            throw new bc.InvalidBytecodeException("Error: Invalid byte code")
+          }
+        } else {
+          if (validateByteCode(field)) {
+            byteCodes ++= byteCodeParser.parse(Vector(bytecode(fieldOne)))
+          }else{
+            //An exception representing an invalid bytecode is thrown
+            throw new bc.InvalidBytecodeException("Error: Invalid byte code")
           }
 
         }
 
       }
+
+    }
     byteCodes
   }
 
